e6959914807511191e1ae0eb08b36fa65dda4347
Matias Suter <sutermatias9@gmail.com>
2024-03-06 20:24:44 -0300

    Task History created.
    - Implemented task history, it needs styling but its working.
    - ldsUtils.js added to format Dates and error handling.

--- force-app/main/default/lwc/completedTasksHistory/completedTasksHistory.js
+++ force-app/main/default/lwc/completedTasksHistory/completedTasksHistory.js
@@ -1,16 +1,40 @@
 import { LightningElement, wire } from 'lwc';
+import { formatDate } from 'c/ldsUtils';
 import getCompletedTasks from '@salesforce/apex/TaskHandler.getCompletedTasks';
 
 export default class CompletedTasksHistory extends LightningElement {
-    completedTasks = [];
+    completedTasks;
+    tasksByDate;
+    dates;
 
     @wire(getCompletedTasks)
     completedTasksWired({ data, error }) {
         if (data) {
             this.completedTasks = data;
             console.log('completed tasks: ' + JSON.stringify(this.completedTasks));
+            this.createTaskHistory();
         } else if (error) {
             console.error(error);
+        }
+    }
+
+    createTaskHistory() {
+        if (this.completedTasks) {
+            this.tasksByDate = this.completedTasks.reduce((array, currentTask) => {
+                const date = formatDate(currentTask.ActivityDate);
+
+                const dateEntry = array.find((entry) => entry.date === date);
+
+                if (dateEntry) {
+                    dateEntry.tasks.push(currentTask.Subject);
+                } else {
+                    array.push({ date, tasks: [currentTask.Subject] });
+                }
+
+                return array;
+            }, []);
+
+            console.log('tbd: ' + JSON.stringify(this.tasksByDate));
         }
     }
 }
